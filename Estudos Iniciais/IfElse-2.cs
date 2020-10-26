1815657e2f8cd9ce2e8fd6cd07f4dac4716ced1e
DicoDuprat <53021921+dicoduprat@users.noreply.github.com>
2020-10-26 14:42:58 -0300

    Teste
    Aulas

---
+++ Estudos Iniciais/IfElse-2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Aula_2___IfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * O professor gostaria de saber se está gostando das aulas
+             * se alguém não gostar 
+             * quer saber o motivo também 
+             */
+
+            String EstadoAluno;
+            String RespostaSeNao;
+
+            Console.WriteLine("Você está gostando das aulas apresentadas?(Responda com SIM ou NÃO)");
+            EstadoAluno = Console.ReadLine();
+            // condicional - == sim
+            //ToLower - mesmo que a pessoa responda com capslock ainda irá funcionar
+            if (EstadoAluno.ToLower() == "sim")
+            {
+                Console.WriteLine("Ficamos felizes em saber que as aulas estão te agradando");
+            }
+            else
+            {//evitar que qualquer coisas diferente de SIM estrague o programa
+                if (EstadoAluno.ToLower() == "não")
+                {//pesquisa para saber o motivo
+                    Console.WriteLine("Que pena, poderia nos dizer qual é motivo?");
+                    //capturar o que for respondido
+                    RespostaSeNao = Console.ReadLine();
+                    //Printar a resposta do aluno
+                    Console.WriteLine("Motivo: " + RespostaSeNao);
+                }
+            }
+            //para o programa não fechar
+            Console.ReadKey();
+
+        }
+    }
+ }
---
+++ Estudos Iniciais/WriteLine-1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Aula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // inputar informação - criando variável
+            String Sonho; 
+
+
+            // Separa em linhas o que for escrito
+            Console.WriteLine("Olá Mundo!");
+
+            // Deita printado tudo na mesma linha / sem organização alguma
+            Console.Write("Qual seu maior sonho?");
+
+            // Pega o que for escrito de resposta
+            Condicao = Console.ReadLine();
+
+            // printar resposta buscando a variável que foi escrita
+            Console.WriteLine("Seu maior sonho é:" + Sonho);
+
+
+         /* O programa só será fechado se alguma tecla for pressionada
+          * 
+          * Pode colocar parâmetros como boelano e etc... 
+          * 
+         */
+
+            Console.ReadKey();
+
+        }
+    }
+}
